c4b5b7de5c1b31d4585522b714992139f4cb4183
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-09-06 11:50:33 -0400

    stopwatch working

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor() {
+  super()
+  this.state = {
+    time: 0,
+    currentTime: null
+  }
+  this.startClick = this.startClick.bind(this)
+  this.pauseClick = this.pauseClick.bind(this)
+  this.resetClick = this.resetClick.bind(this)
+}
+startClick () {
+  console.log("starting clock")
+  this.state.currentTime = setInterval(() => {
+    this.setState({time: this.state.time + 1})}, 1000)
+  }
+pauseClick () {
+  console.log("pausing clock")
+  clearInterval(this.state.currentTime)
+}
+resetClick () {
+  console.log("resetting clock")
+  clearInterval(this.state.currentTime)
+  this.setState({ time: 0 })
+}
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.time}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this.resetClick}>Reset</button>
+          <button onClick={this.startClick}>Start</button>
+          <button onClick={this.pauseClick}>Pause</button>
         </div>
       </div>
     );
